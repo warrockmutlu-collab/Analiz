7158f390010cf20529dae3ae3a6f1fe29f927a90
warrockmutlu-collab <warrockmutlu@gmail.com>
2026-04-03 21:06:49 +0300

    Create MatchAnalysis.jsx component for football match analysis

---
+++ src/components/MatchAnalysis.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MatchAnalysis = () => {
+    return (
+        <div>
+            <h1>Football Match Analysis</h1>
+            <h2>Match Predictions</h2>
+            <p>Details about the match analysis and predictions will be displayed here.</p>
+            {/* Add further analysis and predictions logic here */}
+        </div>
+    );
+};
+
+export default MatchAnalysis;
